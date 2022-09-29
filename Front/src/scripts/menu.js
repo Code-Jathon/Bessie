fede341960be8add3a55d7f248a8fde528021700
Juan Jose Mazo Acevedo <juan.mazoac@amigo.edu.co>
2022-09-28 22:28:47 -0500

    Enviando nuevo registro de articulo a Firebase

--- Front/src/scripts/menu.js
+++ Front/src/scripts/menu.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+
   const db = firebase.firestore();
 
   const getData = async () => {
@@ -10,11 +11,9 @@ $(document).ready(() => {
         querySnapshot.forEach((doc) => {
           daticos.push(doc.data().article);
         });
-      })  
+      })
       .then(() => daticos);
   };
-
- 
 
   const iconoEditar =
     '<svg class="bi bi-pencil-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg>';
@@ -85,6 +84,8 @@ $(document).ready(() => {
     document.getElementById("tBody").appendChild(tr);
   }
 
+
+
   (async () => {
     const data = await getData();
     console.log(data);
@@ -95,7 +96,8 @@ $(document).ready(() => {
     });
     //Todos los campos
     //table para pagination
-    let table = $("mydatatable").DataTable({
+    /*
+    var table = $("mydatatable").DataTable({
       pageLength: 5,
       lengthMenu: [
         [5, 10, 20, -1],
@@ -114,22 +116,24 @@ $(document).ready(() => {
         },
       ],
     });
+    */
   })();
 });
+
 
 const openEls = document.querySelectorAll("[data-open]");
 const closeEls = document.querySelectorAll("[data-close]");
 const isVisible = "is-visible";
 
 for (const el of openEls) {
-  el.addEventListener("click", function() {
+  el.addEventListener("click", function () {
     const modalId = this.dataset.open;
     document.getElementById(modalId).classList.add(isVisible);
   });
 }
 
 for (const el of closeEls) {
-  el.addEventListener("click", function() {
+  el.addEventListener("click", function () {
     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
   });
 }
@@ -148,21 +152,21 @@ document.addEventListener("keyup", e => {
 });
 
 //JS MODAL
-if(document.getElementById("btnNuevo")){
+if (document.getElementById("btnNuevo")) {
   var modal = document.getElementById("myModal");
   var btn = document.getElementById("btnNuevo");
   var span = document.getElementsByClassName("close")[0];
   var body = document.getElementsByTagName("body")[0];
 
-  btn.onclick = function() {
+  btn.onclick = function () {
     modal.style.display = "block";
 
     body.style.position = "static";
     body.style.height = "100%";
     body.style.overflow = "hidden";
   }
-  
-  span.onclick = function() {
+
+  span.onclick = function () {
     modal.style.display = "none";
 
     body.style.position = "inherit";
@@ -170,7 +174,7 @@ if(document.getElementById("btnNuevo")){
     body.style.overflow = "visible";
   }
 
-window.onclick = function(event) {
+  window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
 
@@ -181,139 +185,20 @@ window.onclick = function(event) {
   }
 
 }
-//JS MODAL
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-//FUNCIONES FIREBASE
-
-var db =firebase.database();
-
-var coleccionProductos = db.ref().child("articles");
-
-
-coleccionProductos.on("child_added", datos => {    ///AGREGAR    
-  dataSet = [datos.key, datos.child("1").val(), datos.child("2").val(), datos.child("3").val(), datos.child("4").val(),
-  datos.child("5").val(), datos.child("6").val(), datos.child("7").val(), datos.child("8").val(), datos.child("9").val(),
-  datos.child("10").val(), datos.child("11").val(), datos.child("12").val(), datos.child("13").val(), datos.child("14").val(),
-  datos.child("15").val(), datos.child("16").val(), datos.child("17").val(), datos.child("18").val(), datos.child("19").val(),
-  datos.child("20").val(), datos.child("21").val(), datos.child("22").val(), datos.child("23").val(), datos.child("24").val(),
-  datos.child("25").val(), datos.child("26").val(), datos.child("27").val(), datos.child("28").val(), datos.child("29").val(),
-  datos.child("30").val(), datos.child("31").val(), datos.child("32").val(), datos.child("33").val(), datos.child("34").val(),
-  datos.child("35").val(), datos.child("36").val(), datos.child("37").val(), datos.child("38").val(), datos.child("39").val(),
-  datos.child("40").val(), datos.child("41").val(), datos.child("42").val(), datos.child("43").val(), datos.child("44").val(),
-  datos.child("45").val(), datos.child("46").val(), datos.child("47").val(), datos.child("48").val(), datos.child("49").val(),
-  datos.child("50").val(), datos.child("51").val(), datos.child("52").val(), datos.child("53").val()];
-  table.rows.add([dataSet]).draw();
-});
-
-
-coleccionProductos.on('child_changed', datos => {    //ACTUALIZAR/EDITAR        
-  dataSet = [datos.key, datos.child("1").val(), datos.child("2").val(), datos.child("3").val(), datos.child("4").val(),
-  datos.child("5").val(), datos.child("6").val(), datos.child("7").val(), datos.child("8").val(), datos.child("9").val(),
-  datos.child("10").val(), datos.child("11").val(), datos.child("12").val(), datos.child("13").val(), datos.child("14").val(),
-  datos.child("15").val(), datos.child("16").val(), datos.child("17").val(), datos.child("18").val(), datos.child("19").val(),
-  datos.child("20").val(), datos.child("21").val(), datos.child("22").val(), datos.child("23").val(), datos.child("24").val(),
-  datos.child("25").val(), datos.child("26").val(), datos.child("27").val(), datos.child("28").val(), datos.child("29").val(),
-  datos.child("30").val(), datos.child("31").val(), datos.child("32").val(), datos.child("33").val(), datos.child("34").val(),
-  datos.child("35").val(), datos.child("36").val(), datos.child("37").val(), datos.child("38").val(), datos.child("39").val(),
-  datos.child("40").val(), datos.child("41").val(), datos.child("42").val(), datos.child("43").val(), datos.child("44").val(),
-  datos.child("45").val(), datos.child("46").val(), datos.child("47").val(), datos.child("48").val(), datos.child("49").val(),
-  datos.child("50").val(), datos.child("51").val(), datos.child("52").val(), datos.child("53").val()];
-  table.row(filaEditada).data(dataSet).draw();
-});
-
-coleccionProductos.on("child_removed", function() { //ELIMINAR
-  table.row(filaEliminada.parents('tr')).remove().draw();                     
-});
-
-
-
-$('form').submit(function(e){                         
-  e.preventDefault();
-  let id = $.trim($('#id').val());      
-  let ESTADO = $.trim($('#1').val());
-  let TÍTULO = $.trim($('#2').val());         
-  let LIDER= $.trim($('#3').val());
-  let TIPOPUBLI = $.trim($('#4').val());
-  let INSTIEDITORA = $.trim($('#5').val());
-  let RECEPROPUESTA = $.trim($('#6').val());
-  let CDASR = $.trim($('#7').val());
-  let CDC = $.trim($('#8').val());
-  let AADACE = $.trim($('#9').val());
-  let SADO = $.trim($('#10').val());
-  let RDSCPE = $.trim($('#11').val());
-  let RDSCSE = $.trim($('#12').val());
-  let FPE = $.trim($('#13').val());
-  let FSE = $.trim($('#14').val());
-  let FTE = $.trim($('#15').val());
-  let IRVPBE = $.trim($('#16').val());
-  let RAAACE = $.trim($('#17').val());
-  let RDSCTE = $.trim($('#18').val());
-  let IRVCTE = $.trim($('#19').val());
-  let RDATE = $.trim($('#20').val());
-  let ICE = $.trim($('#21').val());
-  let EALACCE = $.trim($('#22').val());
-  let ACLVCE = $.trim($('#23').val());
-  let SDD = $.trim($('#24').val());
-  let MDC = $.trim($('#25').val());
-  let ADC = $.trim($('#26').val());
-  let MDCD = $.trim($('#27').val());
-  let OAM = $.trim($('#28').val());
-  let REVE = $.trim($('#29').val());
-  let SDI = $.trim($('#30').val());
-  let ADI = $.trim($('#31').val());
-  let SDC = $.trim($('#32').val());
-  let ADCA = $.trim($('#33').val());
-  let ADU = $.trim($('#34').val());
-  let SADOR = $.trim($('#35').val());
-  let PUB = $.trim($('#36').val());
-  let DL = $.trim($('#37').val());
-  let OBS = $.trim($('#38').val());
-  let ASLO = $.trim($('#39').val());
-  let CDADO = $.trim($('#40').val());
-  let TAAPAO = $.trim($('#41').val());
-  let FCDE = $.trim($('#42').val());
-  let NT = $.trim($('#43').val());
-  let RDR = $.trim($('#44').val());
-  let FLEDA = $.trim($('#45').val());
-  let EBDCDC = $.trim($('#46').val());
-  let ODLCAC = $.trim($('#47').val());
-  let CDDBH = $.trim($('#48').val());
-  let CFPUCLA = $.trim($('#49').val());                        
-  let CFPU = $.trim($('#50').val());
-  let OYADC = $.trim($('#51').val());
-  let REU = $.trim($('#52').val());
-  let ADTDCE = $.trim($('#53').val());
-  let idFirebase = id;        
-  if (idFirebase == ''){                      
-      idFirebase = coleccionProductos.push().key;
-  };                
-  data = {"ESTADO":ESTADO, "TÍTULO":TÍTULO, "LIDER DE LA PUBLICACION":LIDER, "TIPO DE PUBLICACIÓN":TIPOPUBLI, "INSTITUCIÓN EDITORA":INSTIEDITORA,
- "RECEPCIÓN DE LA PROPUESTA DE PUBLICACIÓN":RECEPROPUESTA, "COMUNICACIÓN DE ANÁLISIS DE ORIGINALIDAD":CDASR,
- "CONVENIO DE COEDICIÓN":CDC, "ARCHIVOS AJUSTADOS DE ACUERDO CON EL ANÁLISIS DE ORIGINALIDAD":AADACE, "SEGUNDO ANÁLISIS DE ORIGINALIDAD": SADO,
- "RADICACIÓN DE SOLICITUD CONTRATO PRIMER EVALUADOR":RDSCPE,"RADICACIÓN DE SOLICITUD CONTRATO SEGUNDO EVALUADOR": RDSCSE, "FINALIZA PRIMERA EVALUACIÓN":FPE,
- "FINALIZA SEGUNDA EVALUACIÓN":FSE, "FINALIZA TERCERA EVALUACIÓN":FTE,"INFORME DE RESULTADOS DE VIABILIDAD DE LA PROPUESTA CON BASE EN LAS EVALUACIONES":IRVPBE,
- "RECEPCIÓN DE ARCHIVOS AJUSTADOS DE ACUERDO CON LAS EVALUACIONES":RAAACE, "RADICACIÓN DE SOLICITUD CONTRATO TERCERA EVALUACIÓN":RDSCTE,
- "INFORME DE RESULTADOS DE VIABILIDAD CON BASE EN TRES EVALUACIONES":IRVCTE, "RECEPCIÓN DE ARCHIVOS AJUSTADOS DE ACUERDO CON LA TERCERA EVALUACIÓN":RDATE,
- "INICIO CORRECCIÓN DE ESTILO":ICE, "ENTREGA AL LÍDER DE ARCHIVOS CON CORRECCIÓN DE ESTILO":EALACCE, "ARCHIVOS CON LA VALIDACIÓN DE LA CORRECCIÓN DE ESTILO": ACLVCE,
- "SOLICITUD DE DISEÑO":SDD,"MUESTRA DE CARÁTULA":MDC,"APROBACIÓN DE CARÁTULA":ADC, "MUESTRA DE CONTENIDO DISEÑADO":MDCD, "OBSERVACIONES A LAS MUESTRAS":OAM,
- "REVISIÓN EDITORIAL (VERIFICACIÓN EDITORIAL)":REVE,"SOLICITUD DE ISBN":SDI, "ASIGNACIÓN DE ISBN":ADI, "SOLICITUD DE CATALOGACIÓN":SDC, "ASIGNACIÓN DE CATALOGACION":ADCA,
- "APROBACIÓN DEL AUTOR":ADU, "SEGUNDO ANÁLISIS DE ORIGINALIDAD":SADOR, "PUBLICACION":PUB, "DEPÓSITOS LEGALES":DL,"OBSERVACIONES": OBS, "ACLARACIÓN DE DERECHOS SOBRE LA OBRA":ASLO,
- "COMUNICACIÓN DE ANALISIS DE ORIGINALIDAD(LUEGO DE LOS AJUSTES)":CDADO, "TEXTO AJUSTADO A PARTIR DEL ANÁLISIS DE ORIGINALIDAD":TAAPAO, "FINALIZA CORRECIÓN DE ESTILO": FCDE,
- "NUEVO TITULO": NT, "REMISION DE REVISION": RDR, "FECHA LIMITE ENTREGA DE AJUSTES":FLEDA, "ENVÍO BORRADOR DE CONVENIO DE COEDICIÓN": EBDCDC, "OBSERVACIONES DE LOS COEDITORES AL CONTRATO":ODLCAC,
- "CARTA DE DESISTIMIENTO DE BIEN HUMANO":CDDBH, "CONVENIO FIRMADO POR LA UNIVERSIDAD CATÓLICA LUIS AMIGÓ":CFPUCLA, "CONVENIO FIRMADO POR LAS UNIVERSIDADES Y CARTA DE BIEN HUMANO":CFPU,
- "OBSERVACIONES Y AJUSTES DE CORRECCIÓN":OYADC, "REUNION ENTRE UNIVERSIDADES":REU,"ACUERDO DE TERMINOS DE DISEÑO CON COORDINADORA EDITORIAL":ADTDCE
-};
-  actualizacionData = {};
-  actualizacionData[`/${idFirebase}`] = data;
-  coleccionProductos.update(actualizacionData);
-  id = '';        
-  $("form").trigger("reset");
-  $('#myModal').modal('hide');
-});
+//JS MODAL FIN
 
 
 
 
 
 
-   
+
+
+
+
+
+
+
+
+
+
