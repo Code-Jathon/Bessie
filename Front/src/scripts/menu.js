7be32bf3ca8149561644689c6dedae191604c9da
Sneyder M <sneyder.martinez@wiga.io>
2022-10-02 11:45:46 -0500

    Muestra de visualizacion de la data de Firebase in DT

--- Front/src/scripts/menu.js
+++ Front/src/scripts/menu.js
@@ -1,12 +1,37 @@
 console.log("menu.js loaded");
 
-const $db = firebase.firestore();
+const $db = firebase.firestore().collection("articles");
 
-$db.collection("articles").onSnapshot(snapshot => {
-    let articulos = [];
-    snapshot.forEach(element => {
-        articulos.push({...element.data(), id: element.id});
+let articulos = new Array();
+
+// $db.collection("articles").onSnapshot(snapshot => {
+//     // let articulos = new Array();
+//     snapshot.forEach(element => {
+//         articulos.push({...element.data(), id: element.id});
+//     });
+
+//     console.log("Cantidad de articulos: ", articulos.length);
+//     console.log("Primer articulo desde firebase: ", articulos[0].article);
+
+//     // $('#FondoEditorial-Firebase').DataTable({
+//     //     data: articulos,
+//     // })
+// }) 
+
+$db.get().then(function (querySnapshot) {
+    querySnapshot.forEach(doc => {
+        // console.warn("Articulo: ", doc.data());
+        // debugger
+        console.log(doc.data().article['TÍTULO']);
+        document.getElementById("FondoEditorial-Firebase").innerHTML += "<tbody> <tr> <td>" + doc.data().article['TÍTULO'] + "</td> </tr> </tbody>";
     });
+})
 
-    console.log("Articulos: ", articulos);
+console.log("Hiii");
+$db.get().then((snapshot) => {
+    const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    }));
+    console.warn("Todos los articulos en firebase: ", data);
 })
