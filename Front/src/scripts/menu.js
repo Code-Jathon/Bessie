860bc0188bfe787609969ca4c0db6a484960ec41
Bryan_Arias <bryanariasq02@gmail.com>
2022-10-06 13:00:17 -0500

    Fix NavBar
    Add navigator for data-table and graph

--- Front/src/scripts/menu.js
+++ Front/src/scripts/menu.js
@@ -8,13 +8,16 @@ $db.collection("articles").onSnapshot(snapshot => {
     snapshot.forEach(element => {
         articulos.push({...element.data(), id: element.id});
     });
-
+    
     console.log("Cantidad de articulos: ", articulos.length);
     console.log("Primer articulo desde firebase: ", articulos[0].article);
-
+    
     // debugger
     $('#FondoEditorial-Firebase').DataTable({
         "pageLength": 10,
+        //dom: 'Bfrtip',
+        //buttons: [
+        //    'copy', 'csv', 'excel', 'pdf'],
         data: articulos,
         "columns": [
             { data: "id", "title": "ID"},
@@ -76,7 +79,8 @@ $db.collection("articles").onSnapshot(snapshot => {
         // ordering: false,
         // order: [[0, 'asc']],
         scrollX: true,
-        scrollCollapse: true,
+        scrollY: 400,
+        //scrollCollapse: true,
         fixedColumns: true,
         "language": {
             "search": "Buscador: ",
@@ -94,3 +98,42 @@ $db.collection("articles").onSnapshot(snapshot => {
         }
     })
 })
+
+document.addEventListener("DOMContentLoaded", function(event) {
+   
+    const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId)
+    
+    // Validate that all variables exist
+    if(toggle && nav && bodypd && headerpd){
+    toggle.addEventListener('click', ()=>{
+    // show navbar
+    nav.classList.toggle('show')
+    // change icon
+    toggle.classList.toggle('bx-x')
+    // add padding to body
+    bodypd.classList.toggle('body-pd')
+    // add padding to header
+    headerpd.classList.toggle('body-pd')
+    })
+    }
+    }
+    
+    showNavbar('header-toggle','nav-bar','body-pd','header')
+    
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link')
+    
+    function colorLink(){
+    if(linkColor){
+    linkColor.forEach(l=> l.classList.remove('active'))
+    this.classList.add('active')
+    }
+    }
+    linkColor.forEach(l=> l.addEventListener('click', colorLink))
+    
+     // Your code to run since DOM is loaded and ready
+    });
